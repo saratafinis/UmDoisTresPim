7257b6cd99cda4e6f9f3b64cebbdc1898c812c84
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-12-03 21:54:10 -0300

    Versão inicial

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o número máximo para o jogo 'Um Dois Três Pim':");
+        int maxNumber;
+ 
+        // Verifica se a entrada é um número válido
+        if (int.TryParse(Console.ReadLine(), out maxNumber))
+        {
+            for (int i = 1; i <= maxNumber; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Pim");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Por favor, insira um número válido.");
+        }
+    }
+}
